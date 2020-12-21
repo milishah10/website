f3cb0c4636802ebe814086136e7ce079acdcef40
Mili Shah <milishah1097@gmail.com>
2020-12-21 17:15:42 +0530

    Update Project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -23,6 +23,27 @@ export default function ProjectsPage() {
                   enterprise app solution 2020
                 </a>
               </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/laravel-web-development-for-enterprise-apps//">
+                  Laravel web development for enterprise apps
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/compelling-reasons-to-choose-laravel-web-development-company/">
+                  Compelling reasons to choose laravel web development company
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/how-ai-can-enhance-the-laravel-web-development/">
+                  How AI can enhance the Laravel web development
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/how-the-laravel-framework-is-highly-preferable-to-strengthen-business-growth/">
+                  Why Laravel framework is highly preferable 
+                  to strengthen business growth
+                </a>
+              </li>
             </>
           ),
         },
@@ -74,10 +95,9 @@ export default function ProjectsPage() {
           description: (
             <>
               <li>
-                <a href="https://www.ncodetechnologies.com/how-laravel-development-company-can-help-you-build-an-enterprise-app-solution-2020/">
-                  How laravel development company can help you build an
-                  enterprise app solution 2020
-                </a>
+                <a href="https://www.ncodetechnologies.com/why-erp-is-essential-for-manufacturing-operations/">
+                  Why ERP is essential for manufacturing operations
+                </a>  
               </li>
             </>
           ),
@@ -87,9 +107,32 @@ export default function ProjectsPage() {
           description: (
             <>
               <li>
-                <a href="https://www.ncodetechnologies.com/how-laravel-development-company-can-help-you-build-an-enterprise-app-solution-2020/">
-                  How laravel development company can help you build an
-                  enterprise app solution 2020
+                <a href="https://www.ncodetechnologies.com/The+Call+for+Customize+Checkout+Page+for+Shopify+Web+Development/">
+                  The call for Customize Checkout page for 
+                  Shopify web Development
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/managing-multiple-custom-designs-with-shopify-online-poster-design-tool/">
+                  Managing multiple custome designs with 
+                  shopify online poster design tool
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/shopify-plus-serving-international-brands-with-its-stand-out-features/">
+                  Shopify plus serving international brands with 
+                  its stand out features
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/customize-banner-design-ultimate-solution-provided-by-shopify-development-company/">
+                  Customize banner design ultimate solution
+                  provided by Shopify development company
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/unknown-facts-of-shopify-ecommerce-store-sevelopment/">
+                  Unknown facts of shopify e-commerce store development
                 </a>
               </li>
             </>
@@ -100,9 +143,308 @@ export default function ProjectsPage() {
           description: (
             <>
               <li>
-                <a href="https://www.ncodetechnologies.com/how-laravel-development-company-can-help-you-build-an-enterprise-app-solution-2020/">
-                  How laravel development company can help you build an
-                  enterprise app solution 2020
+                <a href="https://www.ncodetechnologies.com/how-wordpress-web-stories-plugins-could-get-you-on-the-today-show/">
+                  How wordpress web stories plugins
+                  could get you on the today show
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/the-amendments-of-the-wordpress-new-version-5-5/">
+                  The amendments of wordpress new version 5.5
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/how-to-develop-a-wordpress-website-as-a-saas/">
+                  How to develop a wordpress website as a saas
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/things-to-consider-in-wordpress-website-optimization/">
+                  things to consider in wordpress website optimization
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/perks-you-can-get-with-wordpress-web-development-company/">
+                  Perks you can get with wordpress web development company
+                </a>
+              </li>
+              <li>
+                <a href="https://www.wordpresssupporthub.com/">
+                  FAQ - wordpress support hub 
+                </a>
+              </li>
+            </>
+          ),
+        },
+        'web-development': {
+          label: 'The world’s most popular website builder.',
+          description: (
+            <>
+              <li>
+                <a href="https://www.ncodetechnologies.com/magnificent-php-web-development-trends-2020-to-endorse/">
+                  magnificent PHP web development trends 2020 to endorse
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/the-importance-of-python-programming-language/">
+                  Importance of python programming language
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/importance-of-page-speed-in-ecommerce-conversion-rate/">
+                  Importance of page speed in e-commmerce conversion rate
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/how-web-development-can-help-your-business-in-coronavirus/">
+                  How web development can help your business in coronavirus
+                </a>
+              </li>
+            </>
+          ),
+        },
+        iot: {
+          label: 'The world’s most popular website builder.',
+          description: (
+            <>
+              <li>
+                <a href="https://www.esparkinfo.com/node-js-for-iot.html/">
+                  nodejs for IoT
+                </a>
+              </li>
+            </>
+          ),
+        },
+        'digital-marketing': {
+          label: 'The world’s most popular website builder.',
+          description: (
+            <>
+              <li>
+                <a href="http://www.smsidea.biz/Service/SMM-services/">
+                  Web site content  - Social media Marketing services
+                </a>
+              </li>
+              <li>
+                <a href="http://www.smsidea.biz/Service/SMO-services/">
+                  Web site content  - SMO services
+                </a>
+              </li>
+              <li>
+                <a href="http://wizardseohub.com/services/social-media-optimization//">
+                  Web site content  - Social Media Optimization services
+                </a>
+              </li>
+            </>
+          ),
+        },
+        pwa: {
+          label: 'The world’s most popular website builder.',
+          description: (
+            <>
+              <li>
+                <a href="https://www.ncodetechnologies.com/insights-of-progressive-web-app-development//">
+                  insights of progressive web app development
+                </a>
+              </li>
+            </>
+          ),
+        },
+        ' devops': {
+          label: 'The world’s most popular website builder.',
+          description: (
+            <>
+              <li>
+                <a href="https://www.ncodetechnologies.com/devops-software-development-services-survivor-of-covid-19/">
+                  Devops software development services 
+                  survivor of COVID-19
+                </a>
+              </li>
+            </>
+          ),
+        },
+        'nodejs': {
+          label: 'The world’s most popular website builder.',
+          description: (
+            <>
+              <li>
+                <a href="https://www.ncodetechnologies.com/ruling-node-js-frameworks-of-2020/">
+                  Ruling Node Js frameworks of 2020
+                </a>
+              </li>
+            </>
+          ),
+        },
+        'magento': {
+          label: 'The world’s most popular website builder.',
+          description: (
+            <>
+              <li>
+                <a href="https://www.ncodetechnologies.com/ncode-technologies-as-a-guide-for-magento-2-4/">
+                  NCode technologies as a guide for magento 2.4
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/magento-extension-to-stand-ahead-of-the-competition/">
+                  Magento extension to stand ahead of the competition
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/how-magento-can-accelerate-your-b2b-business-at-the-global-level/">
+                  How Magento can accelerate your B2B business
+                  at the global level
+                </a>
+              </li>
+            </>
+          ),
+        },
+        'nop-commerce': {
+          label: 'The world’s most popular website builder.',
+          description: (
+            <>
+              <li>
+                <a href="https://www.ncodetechnologies.com/how-nopcommerce-development-services-your-go-to-choice-for-b2b-development/">
+                  How nopcommerce development services your
+                  go to choice for B2B development
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/the-signification-of-multi-vendor-nopcommerce-e-commerce-web-store/">
+                  The signification of multi vendor nopcommerce
+                  e-commerce web store
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/how-to-decrease-the-cart-abandonment-rate-for-your-nopcommerce-website/">
+                  How to decrease the cart abandonment rate
+                  for your NopCommerce website
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/nopcommerce-4-3-the-newer-the-better/">
+                  NopCommerce 4.3 the newer the better
+                </a>
+              </li>
+            </>
+          ),
+        },
+        'woo-commerce': {
+          label: 'The world’s most popular website builder.',
+          description: (
+            <>
+              <li>
+                <a href="https://www.ncodetechnologies.com/woocommerce-plugins-to-choose-for-better-audience-engagement/">
+                  woocommerce plugins to choose for better audience
+                  engagement
+                </a>
+              </li>
+            </>
+          ),
+        },
+        'codeigniter': {
+          label: 'The world’s most popular website builder.',
+          description: (
+            <>
+              <li>
+                <a href="https://www.ncodetechnologies.com/death-of-codeigniter-web-development-is-that-true/">
+                  Death of codeigniter web development is that true
+                </a>
+              </li>
+              <li>
+                <a href="https://www.ncodetechnologies.com/the-important-aspects-to-check-before-choosing-codeigniter-development-company/">
+                  The important aspects to check before choosing
+                  the codeigniter development company
+                </a>
+              </li>
+            </>
+          ),
+        },
+        'home-decore': {
+          label: 'The world’s most popular website builder.',
+          description: (
+            <>
+              <li>
+                <a href="http://seorank.us/testing/ccs/demo/splashback/">
+                  Webpage content - splashback
+                </a>
+              </li>
+              <li>
+                <a href="http://seorank.us/testing/ccs/demo/shower-screen/">
+                  Webpage content - shower screen
+                </a>
+              </li>
+              <li>
+                <a href="http://seorank.us/testing/ccs/demo/glass-partitioning/">
+                  Webpage content - Glass partitioning
+                </a>
+              </li>
+              <li>
+                <a href="http://seorank.us/testing/ccs/demo/mirrors/">
+                  Webpage content - mirrors
+                </a>
+              </li>
+            </>
+          ),
+        },
+        'cyber-security': {
+          label: 'The world’s most popular website builder.',
+          description: (
+            <>
+              <li>
+                <a href="https://www.global-technical-support.co.uk/kaspersky-support">
+                  FAQ - kaspersky
+                </a>
+              </li>
+              <li>
+                <a href="https://www.global-technical-support.co.uk/avast-support">
+                  FAQ - avast
+                </a>
+              </li>
+              <li>
+                <a href="https://www.global-technical-support.co.uk/mcafee-support">
+                  FAQ - mcafee
+                </a>
+              </li>
+              <li>
+                <a href="https://www.global-technical-support.co.uk/netgear-support">
+                  FAQ - netgear
+                </a>
+              </li>
+            </>
+          ),
+        },
+        'other': {
+          label: 'The world’s most popular website builder.',
+          description: (
+            <>
+              <li>
+                <a href="https://www.lugelo.com/storybook/feature_inner/i-tried-the-ketogenic-diet-for-30-days-and-heres-what-happened">
+                  my experience on ketogenic diet for 30 days
+                </a>
+              </li>
+              <li>
+                <a href="https://www.lugelo.com/storybook/feature_inner/2020-is-this-the-end-of-travel-blogging-for-me-and-whats-next">
+                  2020 - The end of the travel blogging for me
+                  and what is next
+                </a>
+              </li>
+              <li>
+                <a href="https://www.lugelo.com/storybook/public/a-school-principals-journey-through-the-covid-19-lockdown">
+                  A school principals journey through the COVID-19
+                </a>
+              </li>
+              <li>
+                <a href="https://www.lugelo.com/storybook/public/how-i-teach-my-kid-to-control-their-anger">
+                  How I teach my kid to control their anger
+                </a>
+              </li>
+              <li>
+                <a href="https://www.lugelo.com/storybook/public/how-i-turned-my-workouts-into-my-business">
+                  How I turned my workouts into my business
+                </a>
+              </li>
+              <li>
+                <a href="https://www.lugelo.com/storybook/feature_inner/how-journal-helped-me-to-cherish-my-pregnancy-journey">
+                  How journal helped me to cherish my pregnancy
                 </a>
               </li>
             </>
